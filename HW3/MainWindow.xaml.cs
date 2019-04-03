6ee3c62c2ff1ef5e1c9a5a2b9209bd36e5cb74c1
zbug-nt <2569169388@qq.com>
2019-04-04 01:00:05 +0800

    well

--- HW3/MainWindow.xaml.cs
+++ HW3/MainWindow.xaml.cs
@@ -20,9 +20,75 @@ namespace HW3
     /// </summary>
     public partial class MainWindow : Window
     {
+        String lastLegalAmount = "¥0.00";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private bool IsLegalAmount(String amount)
+        {
+            int cnt = 0, p = -1;
+            if (amount.Length == 0 || amount[0] != '¥') return false;
+            for (int i = 1; i < amount.Length; ++i)
+            {
+                if (amount[i] >= '0' && amount[i] <= '9') ;
+                else if (amount[i] == '.')
+                {
+                    ++cnt;
+                    if (cnt == 2) return false;
+                    p = i;
+                }
+                else return false;
+            }
+            if (p != -1 && amount.Length > p + 3) return false;
+            return true;
+        }
+
+        private void BillAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (!IsLegalAmount(tb.Text)) tb.Text = lastLegalAmount;
+            else lastLegalAmount = tb.Text;
+        }
+
+        private void BillAmountTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            lastLegalAmount = Normalize(lastLegalAmount);
+            tb.Text = lastLegalAmount;
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            PerformCalculation();
+        }
+
+        private void PerformCalculation()
+        {
+            var selectedRadio = mainGrid.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            double bill = Double.Parse(lastLegalAmount.Substring(1)), rate = Double.Parse(selectedRadio.Tag.ToString()), tmp;
+            tmp = bill * rate;
+            amountToTip.Content = Normalize("¥" + Math.Round(tmp, 2));
+            tmp += bill;
+            totalBill.Content = Normalize("¥" + Math.Round(tmp, 2));
+        }
+
+        private String Normalize(String str)
+        {
+            int lastPos = str.LastIndexOf('.'), len = str.Length;
+            String extra = "";
+            if (lastPos == -1) extra = ".00";
+            else if (lastPos == len - 1) extra = "00";
+            else if (lastPos == len - 2) extra = "0";
+            str += extra;
+            return str;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PerformCalculation();
         }
     }
 }
