39b7a78b17d5ec347cf0c509b3297900e8beb7b8
zbug-nt <2569169388@qq.com>
2019-04-04 15:41:17 +0800

    class

--- HW3/MainWindow.xaml.cs
+++ HW3/MainWindow.xaml.cs
@@ -20,44 +20,17 @@ namespace HW3
     /// </summary>
     public partial class MainWindow : Window
     {
-        String lastLegalAmount = "¥0.00";
+        Tip tip;
 
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private bool IsLegalAmount(String amount)
-        {
-            int cnt = 0, p = -1;
-            if (amount.Length == 0 || amount[0] != '¥') return false;
-            for (int i = 1; i < amount.Length; ++i)
-            {
-                if (amount[i] >= '0' && amount[i] <= '9') ;
-                else if (amount[i] == '.')
-                {
-                    ++cnt;
-                    if (cnt == 2) return false;
-                    p = i;
-                }
-                else return false;
-            }
-            if (p != -1 && amount.Length > p + 3) return false;
-            return true;
+            tip = new Tip();
         }
 
         private void BillAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            TextBox tb = (TextBox)sender;
-            if (!IsLegalAmount(tb.Text)) tb.Text = lastLegalAmount;
-            else lastLegalAmount = tb.Text;
-        }
-
-        private void BillAmountTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb = (TextBox)sender;
-            lastLegalAmount = Normalize(lastLegalAmount);
-            tb.Text = lastLegalAmount;
+            PerformCalculation();
         }
 
         private void RadioButton_Click(object sender, RoutedEventArgs e)
@@ -68,27 +41,20 @@ namespace HW3
         private void PerformCalculation()
         {
             var selectedRadio = mainGrid.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
-            double bill = Double.Parse(lastLegalAmount.Substring(1)), rate = Double.Parse(selectedRadio.Tag.ToString()), tmp;
-            tmp = bill * rate;
-            amountToTip.Content = Normalize("¥" + Math.Round(tmp, 2));
-            tmp += bill;
-            totalBill.Content = Normalize("¥" + Math.Round(tmp, 2));
+            if (selectedRadio == null) return;
+            tip.CalculateTip(billAmountTextBox.Text, double.Parse(selectedRadio.Tag.ToString()));
+            amountToTip.Content = tip.TipAmount;
+            totalBill.Content = tip.TotalAmount;
         }
 
-        private String Normalize(String str)
+        private void BillAmountTextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            int lastPos = str.LastIndexOf('.'), len = str.Length;
-            String extra = "";
-            if (lastPos == -1) extra = ".00";
-            else if (lastPos == len - 1) extra = "00";
-            else if (lastPos == len - 2) extra = "0";
-            str += extra;
-            return str;
+            billAmountTextBox.Text = "";
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BillAmountTextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            PerformCalculation();
+            billAmountTextBox.Text = tip.BillAmount;
         }
     }
 }
